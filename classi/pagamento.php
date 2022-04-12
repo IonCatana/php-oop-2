8aeed31b020ddd4b1a6417c1921ef6b58b37af85
IonCatana <catana.ion17@yahoo.it>
2022-04-12 17:11:21 +0200

    pagamento

--- classi/pagamento.php
+++ classi/pagamento.php
@@ -6,7 +6,7 @@ class Pagamento
   protected $dataScadenza;
   protected $nomeCognome;
   protected $cifreValidazione;
-  public $startdate = "12-04-2022"; //data di oggi
+  public $dataOggi = "12-04-2022";
 
   public function __construct($numeroCarta, $dataScadenza, $nomeCognome, $cifreValidazione)
   {
@@ -31,7 +31,46 @@ class Pagamento
   public function setData($data)
   {
     if (preg_match('^([0-2][0-9]|(3)[0-1])(\-||\/)(((0)[0-9])|((1)[0-2]))(\-||\/)\d{4}$', $data)) {
-      $this->data = $data;
+
+      //assegno la data di scadenza correttamente
+      $this->dataScadenza = $data;
+
+      //tramite la funzione strtotime converto la data in un valore numerico
+      $expire = strtotime($this->dataScadenza);
+
+      //se é piú grande significa che é oltre la data di oggi
+      $today = strtotime($this->dataOggi);
+
+      //li confronto
+      if ($expire < $today) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  public function setNomeCognome($nome, $cognome)
+  {
+    //il nome e il cognome non devono essere numerici più corti di 2 caratteri
+    if (!is_numeric($nome) && strlen($nome) > 1 && !is_numeric($cognome) && strlen($cognome) > 1) {
+
+      $this->nome = $nome;
+      $this->cognome = $cognome;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function setValidazione($numeroPin)
+  {
+    //l'utente deve inserire solo 3 numeri del retro della carta
+    if (preg_match('^[0-9]{3}+$', $numeroPin) == 1) {
+
+      $this->cifreValidazione = $numeroPin;
       return true;
     } else {
       return false;
